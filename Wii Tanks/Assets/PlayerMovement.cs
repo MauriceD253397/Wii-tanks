dc9cf708760aaa67de3693a22eb3504d1ff1d99d
Maurice <Mauricederidder@outlook.com>
2019-05-31 17:19:26 +0200

    Initialised project

---
+++ Wii Tanks/Assets/PlayerMovement.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public BoxCollider2D playerCollider;
+    public Rigidbody2D playerBarrel;
+    public Rigidbody2D playerBody;
+    public float speed;
+    private Vector2 bulletForce;
+    private Vector2 forceA;
+    private Vector2 forceS;
+    private Vector2 forceD;
+    public Rigidbody2D bullet;
+    private Rigidbody2D cloneRb;
+    private bool moveBullet;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {   
+        bulletForce = new Vector2(speed *= 2,0);
+
+        //bullet.GetComponent<>
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+
+            if (playerCollider.transform.rotation.z <= 0.70 || playerCollider.transform.rotation.z <= -0.90)
+            {
+               // print(playerCollider.transform.rotation.z);
+                playerCollider.transform.Rotate(0, 0, 1);
+            }
+            else
+            {
+                playerCollider.transform.position += Vector3.up / 5;
+            }
+            
+            //print("W was pressed");
+        }
+        if (Input.GetKey("a"))
+        {
+            playerCollider.transform.position += Vector3.left / 5;
+            // rigidbody2D.position = new Vector2(-1, 0);
+            //   rigidbody2D.AddForce(forceA);
+        }
+        if (Input.GetKey("s"))
+        {
+            if (playerCollider.transform.rotation.z <= 0.75)
+            {
+              //  print(playerCollider.transform.rotation.z);
+                playerCollider.transform.Rotate(0, 0, -1);
+            }
+            else
+            {
+                playerCollider.transform.position += Vector3.down / 5;
+            }
+            
+            
+            // rigidbody2D.position = new Vector2(0, -1);
+            //   rigidbody2D.AddForce(forceS);
+        }
+        if (Input.GetKey("d"))
+        {
+            playerCollider  .transform.position += Vector3.right / 5;
+            // rigidbody2D.position = new Vector2(1, 0);
+            //rigidbody2D.AddForce(forceD);
+        }
+        if (Input.GetKeyDown("q"))
+        {
+            Rigidbody2D cloneRb = Instantiate(bullet, playerBarrel.transform.position, Quaternion.identity) as Rigidbody2D;
+            cloneRb.rotation = Input.mousePosition.x;
+            cloneRb.AddForce(bulletForce);
+
+            //cloneRb.AddForce(playerCollider.transform.forward * 25);
+            //playerBody.AddForce(playerCollider.transform.forward * bulletSpeed);
+            // rigidbody2D.position = new Vector2(1, 0);
+            //rigidbody2D.AddForce(forceD);
+            
+        }
+        
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        print(col.gameObject);
+        if (col.gameObject.tag == "Barricade")
+        {
+            print(col.gameObject.tag);
+            Destroy(col.gameObject);
+        }
+    }
+
+
+}
---
+++ Wii Tanks/Assets/PlayerMovementTutorial.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementTutorial : MonoBehaviour
+{
+    public BoxCollider2D playerCollider;
+    public BoxCollider2D weaponCollider;
+    public Rigidbody2D playerBody;
+    public Rigidbody2D bulletBody;
+    public float movementSpeed;
+    public Vector2 bulletSpeed;
+    private int shootLoop = 0;
+    public int fireRate = 50;
+    Vector3 positionOfMouse;
+    Quaternion rotationOfMouse;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shootLoop == fireRate) shootLoop = 0;
+        else if (shootLoop > 0) shootLoop += 1;
+        if (Input.GetKey("w"))
+        {
+            playerCollider.transform.position += new Vector3(0,movementSpeed);
+        }
+        if (Input.GetKey("s"))
+        {
+            playerCollider.transform.position += new Vector3(0, -movementSpeed);
+        }
+        if (Input.GetKey("a"))
+        {
+            playerCollider.transform.position += new Vector3(movementSpeed, 0);
+        }
+        if (Input.GetKey("d"))
+        {
+            playerCollider.transform.position += new Vector3(-movementSpeed,0);
+        }
+        if (Input.GetMouseButton(0) && shootLoop == 0)
+        {
+            positionOfMouse = Input.mousePosition;
+            rotationOfMouse = new Quaternion(positionOfMouse.x, positionOfMouse.y, positionOfMouse.z, 0);
+            Rigidbody2D bulletClone = Instantiate(bulletBody, playerCollider.transform.position, Quaternion.identity ) as Rigidbody2D;
+            bulletBody.rotation = rotationOfMouse.x;
+            bulletClone.AddForce(bulletSpeed);
+            shootLoop = 1;
+        }
+       positionOfMouse = Input.mousePosition;
+       print(positionOfMouse);
+       weaponCollider.transform.Rotate(positionOfMouse.x,positionOfMouse.y,0);
+
+    }
+    
+}
